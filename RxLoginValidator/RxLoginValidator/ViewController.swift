aad160d6bcf9b4fd1e872b6f9fe8800a1034bcd7
Abhishek Verma <abhishek.verma@emirates.com>
2018-06-09 00:54:05 +0400

    Create login screen

--- RxLoginValidator/RxLoginValidator/ViewController.swift
+++ RxLoginValidator/RxLoginValidator/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
