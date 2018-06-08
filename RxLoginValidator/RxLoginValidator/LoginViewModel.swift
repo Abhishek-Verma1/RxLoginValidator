60c91b6040b346648f059f963d04fe6541d7b19b
Abhishek Verma <abhishek.verma@emirates.com>
2018-06-09 01:02:06 +0400

    Handle userName and password validation

--- RxLoginValidator/RxLoginValidator/LoginViewModel.swift
+++ RxLoginValidator/RxLoginValidator/LoginViewModel.swift
@@ -2,8 +2,23 @@
 //  LoginViewModel.swift
 //  RxLoginValidator
 //
-//  Created by Clean Slate on 6/9/18.
+//  Created by Abhishek Verma on 6/9/18.
 //  Copyright © 2018 None. All rights reserved.
 //
 
 import Foundation
+import RxSwift
+
+struct LoginViewModel {
+    
+    var userNameText = Variable<String>("")
+    var passwordText = Variable<String>("")
+    
+    var isValid: Observable<Bool> {
+        return Observable.combineLatest(userNameText.asObservable(), passwordText.asObservable()) { userName, password in
+            //UserName should be equal and greater than 5, Password should be equal and greater than 6
+            userName.characters.count >= 5 && password.characters.count >= 6
+        }
+    }
+}
+
