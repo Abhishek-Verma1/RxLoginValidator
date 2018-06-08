1db3c22c8c4610c06a81bf4c04a5e413c8856774
Abhishek Verma <abhishek.verma@emirates.com>
2018-06-09 01:06:59 +0400

    Calling Rx validaton in ViewController and managing login button enable/disable

--- RxLoginValidator/RxLoginValidator/ViewController.swift
+++ RxLoginValidator/RxLoginValidator/ViewController.swift
@@ -7,16 +7,31 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
+    
+    var loginViewmodel = LoginViewModel()
+    let disboseBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        _ =  userNameTextField.rx.text.map { $0 ?? "" }.bind(to: loginViewmodel.userNameText)
+        _ =  passwordTextField.rx.text.map { $0 ?? "" }.bind(to: loginViewmodel.passwordText)
+        
+        _ = loginViewmodel.isValid.bind(to: loginButton.rx.isEnabled)
+        
+        loginViewmodel.isValid.subscribe(onNext: { [unowned self] isvalid in
+            self.loginButton.backgroundColor = isvalid ? .red : .gray
+            
+        }).disposed(by: disboseBag)
     }
 
     override func didReceiveMemoryWarning() {
